5cd829176fcbc85edce7a4a2cf9f4fa524febc8f
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2022-03-28 20:04:33 +0100

    get markdown parsing working

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const { marked } = require("marked");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const port = 3000;
+const app = express();
+
+const fs = require("fs");
+const path = require("path");
+
+app.get("*", (req, res) => {
+  const pathName = path.join(__dirname, "/content", req.path, "/index.md");
+
+  fs.readFile(pathName, "utf8", (e, data) => {
+    if (e) {
+      res.send(e.message);
+    } else {
+      const html = marked.parse(data);
+      res.send(html);
+    }
+  });
 });
 
 app.listen(port, () => {
